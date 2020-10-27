fe9485027dd97a2cf22e3a1dbfa4cd431d7def7e
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-10-27 13:21:40 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    def initialize(dogs_name, breed="Mutt")
+        @name = dogs_name
+        @breed = breed
+    end
+    
+    def breed
+        @breed
+    end
+
+    def name
+        @name
+    end
+end
+
+
